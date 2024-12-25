8a642d49b187b35eb398b95322cd5931f7368910
moohammedddd <midorafat115566@gmail.com>
2024-12-25 04:50:49 +0200

    Q 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 19- . Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
+            // System.Console.WriteLine("enter the number of matrix");
+            // int.TryParse(Console.ReadLine(), out int  x);
+            // int [,] Identical = new int [x,x];
+
+            // for (int i = 0; i < Identical.GetLength(0); i++)
+            // {
+            //     for (int j = 0; j < Identical.GetLength(1); j++)
+            //     {
+            //         if (i == j)
+            //         {
+            //             Identical[i,j] = 1;
+            //         }                      
+            //              System.Console.Write(Identical[i,j]);  
+            //     }
+
+            //     System.Console.WriteLine("");
+            // }
+
+            #endregion
+
         }
     }
 }
